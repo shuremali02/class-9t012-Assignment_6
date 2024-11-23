8c855d7073df68d1af80a4cb03cdda5edd0cb46c
Shuremali02 <shuremsyed41@gmail.com>
2024-11-23 03:50:11 -0800

    ypdate

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -30,12 +30,20 @@ export default function Navbar() {
   },]
 
   return (
-    <header className="h-[72px] px-3 md:px-10 py-6 border-[#676767] border-y-[1px] flex items-center bg-gray relative z-50">
+<header className="h-[72px] px-3 md:px-10 py-6 border-[#676767] border-y-[1px] flex items-center bg-gray relative z-50">
   <div className="md:max-w-[1280px] max-w-[428px] w-full h-11 flex justify-between">
     <div className="h-[41px]">
       <Image src={"/images/Logo.svg"} alt="logo" width={139.6} height={30.38} />
     </div>
+    <div className="md:hidden">
+      {/* Hamburger button */}
+      <button onClick={() => setisOpen(!isOpen)}>
+        {isOpen ? <FiX size={28} /> : <FiMenu size={28} />}
+      </button>
+    </div>
   </div>
+
+  {/* Desktop Navigation */}
   <nav className="hidden md:max-w-[910px] w-full h-11 md:gap-8 md:flex md:bg-[#ffffff]">
     <div className="flex md:max-w-[687px] w-full space-x-8 p-[10px] gap-[10px]">
       {Routes.map((route, index) => (
@@ -60,16 +68,11 @@ export default function Navbar() {
       </button>
     </div>
   </nav>
-  <div className="md:hidden">
-    <button onClick={() => setisOpen(!isOpen)}>
-      {isOpen ? <FiX size={28} /> : <FiMenu size={28} />}
-    </button>
-  </div>
+
+  {/* Mobile Navigation */}
   {isOpen && (
-    <nav
-      className="absolute top-full left-0 w-full bg-white shadow-lg items-center flex flex-col gap-y-4 z-50"
-    >
-      <div className="flex flex-col p-[10px] gap-[10px]">
+    <nav className="md:hidden absolute top-full left-0 bg-white z-50 w-full py-4 flex flex-col gap-y-4 items-center">
+      <div className="flex flex-col gap-[10px] px-[10px]">
         {Routes.map((route, index) => (
           <Link href={route.href} key={index}>
             <span
@@ -83,7 +86,7 @@ export default function Navbar() {
         ))}
       </div>
 
-      <div className="flex w-full px-5 gap-3 justify-center">
+      <div className="flex flex-col gap-3 items-center px-4">
         <button className="font-[Roboto] text-[16px] py-2 rounded-[5px] px-5 border-[1px] border-black">
           Login
         </button>
@@ -95,5 +98,7 @@ export default function Navbar() {
   )}
 </header>
 
-  )
-}
+
+
+
+  )}
